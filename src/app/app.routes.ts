cb134b709e012c6bb53d604ed7ffb6a360698ce6
LluisArjuna <lluisarjunav@gmail.com>
2026-03-10 11:08:02 +0100

    feat(app.routes): add the routes to display the different components
    We will change this later do route to the pages created, not directly to the components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ServiceList } from './elements/service-list/service-list';
+import { BudgetForm } from './elements/budget-form/budget-form';
+import { BudgetSummary } from './elements/budget-summary/budget-summary';
+
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: ServiceList
-    }
+    { path: '', component: ServiceList },
+    { path: 'budget-form', component: BudgetForm },
+    { path: 'summary', component: BudgetSummary }
 ];
