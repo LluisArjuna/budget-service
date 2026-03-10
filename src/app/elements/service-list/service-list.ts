ef71eb61d8e7961a230bdc1da68f32e44b78315f
LluisArjuna <lluisarjunav@gmail.com>
2026-03-10 10:54:44 +0100

    feat(service-list): add the generateBudget function where we use the setService function to save the selected services
    We then navigate to /budget-form using the router. We are not using the constructor to call the services. We've updated to the inject() function, more modern.

--- src/app/elements/service-list/service-list.ts
+++ src/app/elements/service-list/service-list.ts
@@ -1,8 +1,9 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { services } from '../../data/services';
 import { Service } from '../../interfaces/service.interface';
 import { ServiceCard } from "../service-card/service-card";
 import { BudgetService } from '../../services/budget-service';    
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-service-list',
@@ -10,10 +11,12 @@ import { BudgetService } from '../../services/budget-service';
   templateUrl: './service-list.html',
   
 })
+
 export class ServiceList {
   services = signal<Service[]>(services);
 
-  constructor(private budgetService: BudgetService) {}
+  private readonly budgetService = inject(BudgetService);
+  private readonly router: Router = inject(Router);
 
   toggleService(id: number) {
   this.services.update(list =>
@@ -46,4 +49,12 @@ export class ServiceList {
     this.budgetService.calculateTotal(this.services())
   );
 
+
+  generateBudget() {
+  const selected = this.services().filter(service => service.selected);
+
+  this.budgetService.setServices(selected);
+
+  this.router.navigate(['/budget-form']);
+}
 }
